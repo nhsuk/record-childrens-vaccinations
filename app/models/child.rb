2aa0f5c4ce2ca8d171a62be73579496be4a2f009
Misha Gorodnitzky <misaka@pobox.com>
2023-01-14 10:15:30 +0000

    Add fields to child model
    These fields have been extracted from the prototype example campaign
    dataset.

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,12 +2,23 @@
 #
 # Table name: children
 #
-#  id         :bigint           not null, primary key
-#  dob        :date
-#  name       :string
-#  nhs_number :decimal(, )
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id             :bigint           not null, primary key
+#  consent        :integer
+#  dob            :date
+#  first_name     :text
+#  gp             :integer
+#  last_name      :text
+#  nhs_number     :decimal(, )
+#  preferred_name :text
+#  screening      :integer
+#  seen           :integer
+#  sex            :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 class Child < ApplicationRecord
+  enum :gp, ['Local GP']
+  enum :screening, ['Approved for vaccination']
+  enum :consent, ['Parental consent (digital)']
+  enum :seen, ['Not yet']
 end
